e8716bdbc95834c203fb88914ecb610a4e0bbe98
pragya5833 <pragyabharti1312@gmail.com>
2024-04-11 17:15:09 +0530

    updated working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project-management-frontend';
+  description: string = '';
+  apiUrl: string = 'http://localhost:3000';
+  responseData: string = ''; // Response data will be displayed here
+  loading: boolean = false;
+
+  constructor(private http: HttpClient) {}
+  generateRequestHeaders() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer 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',
+      }),
+    };
+  }
+  generateUserStories() {
+    this.loading = true; // Set loading to true when the request is initiated
+    const requestBody = { requirement: this.description };
+    this.http
+      .post(
+        `${this.apiUrl}/generate-user-stories`,
+        requestBody,
+        this.generateRequestHeaders()
+      )
+      .subscribe((response: any) => {
+        this.responseData = response.data.replace(/\n/g, '<br>'); // Assign only the 'data' property to responseData;
+        this.loading = false; // Set loading to false when the request is completed
+      });
+  }
+
+  generateAcceptanceCriteria() {
+    this.loading = true; // Set loading to true when the request is initiated
+    const requestBody = { requirement: this.description };
+    this.http
+      .post(
+        `${this.apiUrl}/generate-acceptance-criteria`,
+        requestBody,
+        this.generateRequestHeaders()
+      )
+      .subscribe((response: any) => {
+        this.responseData = response.data.replace(/\n/g, '<br>'); // Assign only the 'data' property to responseData;
+        this.loading = false; // Set loading to false when the request is completed
+      });
+  }
+
+  generateTestCases() {
+    this.loading = true; // Set loading to true when the request is initiated
+    const requestBody = { requirement: this.description };
+    this.http
+      .post(
+        `${this.apiUrl}/generate-test-cases`,
+        requestBody,
+        this.generateRequestHeaders()
+      )
+      .subscribe((response: any) => {
+        this.responseData = response.data.replace(/\n/g, '<br>'); // Assign only the 'data' property to responseData;
+        this.loading = false; // Set loading to false when the request is completed
+      });
+  }
+
+  generatePRD() {
+    this.loading = true; // Set loading to true when the request is initiated
+    const requestBody = { requirement: this.description };
+    this.http
+      .post(
+        `${this.apiUrl}/generate-prd`,
+        requestBody,
+        this.generateRequestHeaders()
+      )
+      .subscribe((response: any) => {
+        this.responseData = response.data.replace(/\n/g, '<br>'); // Assign only the 'data' property to responseData;
+        this.loading = false; // Set loading to false when the request is completed
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, // Include HttpClientModule here
+    FormsModule // Include FormsModule here
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
